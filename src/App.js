405538d76e493d5a9579dbb9f89ff37008da5c60
daracoo <dkuzmanoski21@gmail.com>
2024-07-27 16:03:22 +0200

    The symbols written in the square cannot be changed, Logs under the gameboard with information which postiion the player has chosen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import { useState } from "react"
+import Log from "./components/Log"
 
 function App() {
   const [activePlayer, setActivePlayer] = useState('X')
+  const [gameTurns, setGameTurns] = useState([]);
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X';
+
+      if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
+        currentPlayer = 'O';
+      }
+
+      const updatedTurns = [{ square: { row: rowIndex, col: colIndex}, player: activePlayer }, ...prevTurns];
+      return updatedTurns;
+    });
   }
 
   return (
@@ -16,8 +29,9 @@ function App() {
           <Player initialName="Player1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="Player2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
+      <Log turns={gameTurns}/>
     </main>
   )
 }
